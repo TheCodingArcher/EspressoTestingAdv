12b72450bce3b9b95f85cb808e9ac7075b160a05
TheCodingArcher <rick_thegladiator@yahoo.com>
2019-12-28 13:51:23 +0530

    Incoming Intents: Known Theme test

--- app/src/androidTest/java/android/the/coding/archer/espressotestingadv/IdeasActivityTest.kt
+++ app/src/androidTest/java/android/the/coding/archer/espressotestingadv/IdeasActivityTest.kt
@@ -1,5 +1,6 @@
 package android.the.coding.archer.espressotestingadv
 
+import android.content.Intent
 import android.support.test.espresso.Espresso.onView
 import android.support.test.espresso.assertion.ViewAssertions.matches
 import android.support.test.espresso.matcher.ViewMatchers.withId
@@ -21,5 +22,15 @@ class IdeasActivityTest {
         activityRule.launchActivity(null)
         onView(withId(R.id.theme))
             .check(matches(withText(R.string.missing_theme)))
+    }
+
+    @Test
+    fun punny() {
+        val intent = Intent()
+        intent.putExtra(IdeasActivity.KEY_THEME, "Punny")
+        activityRule.launchActivity(intent)
+
+        onView(withId(R.id.theme))
+            .check(matches(withText("Punny")))
     }
 }
