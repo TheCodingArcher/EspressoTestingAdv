1d0187014e5dee4a4e53c716b5500c4950bb8429
TheCodingArcher <rick_thegladiator@yahoo.com>
2019-12-28 15:01:33 +0530

    Outgoing Intents: How to Match test

--- app/src/androidTest/java/android/the/coding/archer/espressotestingadv/MainActivityTest.kt
+++ app/src/androidTest/java/android/the/coding/archer/espressotestingadv/MainActivityTest.kt
@@ -1,16 +1,22 @@
 package android.the.coding.archer.espressotestingadv
 
+import android.content.Intent
 import android.support.test.InstrumentationRegistry
 import android.support.test.espresso.Espresso.onView
 import android.support.test.espresso.action.ViewActions.click
 import android.support.test.espresso.assertion.ViewAssertions.matches
 import android.support.test.espresso.intent.Intents
+import android.support.test.espresso.intent.matcher.ComponentNameMatchers
+import android.support.test.espresso.intent.matcher.IntentMatchers
 import android.support.test.espresso.intent.matcher.IntentMatchers.hasExtra
 import android.support.test.espresso.intent.rule.IntentsTestRule
 import android.support.test.espresso.matcher.ViewMatchers.withId
 import android.support.test.espresso.matcher.ViewMatchers.withText
 import android.support.test.rule.ActivityTestRule
 import android.support.test.runner.AndroidJUnit4
+import org.hamcrest.Description
+import org.hamcrest.Matcher
+import org.hamcrest.TypeSafeMatcher
 import org.junit.Assert.*
 import org.junit.Rule
 import org.junit.Test
@@ -39,5 +45,8 @@ class MainActivityTest {
         val theme = context.getString(R.string.theme_punny)
 
         Intents.intended(hasExtra(IdeasActivity.KEY_THEME, theme))
+        Intents.intended(IntentMatchers.hasComponent(
+            ComponentNameMatchers.hasClassName("android.the.coding.archer.espressotestingadv.IdeasActivity")
+        ))
     }
 }
