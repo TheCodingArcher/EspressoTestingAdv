82d97fa53008793e0952f2525eb76d2698fa658e
TheCodingArcher <rick_thegladiator@yahoo.com>
2020-01-07 12:12:23 +0530

    Initial Code for Idle Resources

--- app/src/main/java/android/the/coding/archer/espressotestingadv/MainActivity.kt
+++ app/src/main/java/android/the/coding/archer/espressotestingadv/MainActivity.kt
@@ -1,39 +1,25 @@
 package android.the.coding.archer.espressotestingadv
 
-import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.widget.Button
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
-    private val REQUEST_CODE_IDEAS = 1
-    private lateinit var nameView: TextView
+    private lateinit var textView: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        nameView = findViewById(R.id.name)
+        textView = findViewById(R.id.text)
+
+        val fragment = LoadingDialogFragment()
+        fragment.isCancelable = false
+        fragment.show(supportFragmentManager, LoadingDialogFragment.TAG)
     }
 
-    fun goToIdeaActivity(v: View) {
-        val button = v as Button
-
-        val intent = Intent(this, IdeasActivity::class.java)
-        intent.putExtra(IdeasActivity.KEY_THEME, button.text)
-
-        startActivityForResult(intent, REQUEST_CODE_IDEAS)
-    }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        when (requestCode) {
-            REQUEST_CODE_IDEAS -> {
-                nameView.text = data?.getStringExtra(IdeasActivity.KEY_NAME)
-            }
-            else -> super.onActivityResult(requestCode, resultCode, data)
-        }
+    fun onLoadingFinished() {
+        textView.setText(R.string.done)
     }
 }
--- app/src/main/java/android/the/coding/archer/espressotestingadv/TextViewHolder.kt
+++
@@ -1,9 +0,0 @@
-package android.the.coding.archer.espressotestingadv
-
-import android.support.v7.widget.RecyclerView
-import android.view.View
-import android.widget.TextView
-
-class TextViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-    var textView : TextView = itemView as TextView
-}
